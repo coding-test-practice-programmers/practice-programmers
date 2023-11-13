f317812835b27a37d82e8396b9e602e0080c31c1
robin <nowrobin3@gmail.com>
2023-11-14 00:00:39 +0900

    fix han

--- week_3/4_dice_game3/4_Han.js
+++ week_3/4_dice_game3/4_Han.js
@@ -6,44 +6,33 @@ function solution(a, b, c, d) {
   let setlist = [...x];
   let four = 10 * setlist[1] + setlist[0];
   let left = 0;
-  if (x.size === 1) {
-    return a * 1111;
-  } else if (x.size === 4) {
-    return list[0];
-  } else if (x.size === 3) {
-    let j = 0;
-    for (let i = 0; i < L; i++) {
-      if (list[i] !== setlist[i]) {
-        left = list[i];
-        console.log(left);
-        x.delete(left);
-        let temp = [...x];
-        return temp[0] * temp[1];
+
+  switch (x.size) {
+    case 1:
+      return a * 1111;
+
+    case 2:
+      if (list[1] === list[2]) {
+        if (list[1] === setlist[0]) {
+          return Math.pow(10 * setlist[0] + setlist[1], 2);
+        } else if (list[1] === setlist[1]) {
+          return Math.pow(10 * setlist[1] + setlist[0], 2);
+        }
+      } else return (list[0] + list[3]) * (list[3] - list[0]);
+    case 3:
+      let j = 0;
+      for (let i = 0; i < L; i++) {
+        if (list[i] !== setlist[i]) {
+          left = list[i];
+          console.log(left);
+          x.delete(left);
+          let temp = [...x];
+          return temp[0] * temp[1];
+        }
       }
-    }
-  } else if (x.size === 2) {
-    if (list[1] === list[2]) {
-      if (list[1] === setlist[0]) {
-        return Math.pow(10 * setlist[0] + setlist[1], 2);
-      } else if (list[1] === setlist[1]) {
-        return Math.pow(10 * setlist[1] + setlist[0], 2);
-      }
-    } else return (list[0] + list[3]) * (list[3] - list[0]);
+    case 4:
+      return list[0];
   }
-  //     switch(x.size){
-  //         case 1:
-  //             return a*1111
-
-  //         case 2:
-
-  //         case 3:
-
-  //             break;
-  //         case 4:
-  //             return list[0]
-  //             break;
-
-  //     }
 
   return answer;
 }
